92e8cc9d69e5bc8729fbbdb9dda475131640072b
SergeyKirintsev <kserg80@gmail.com>
2021-05-29 17:50:12 +0300

    Добавить обработку сабмита форм

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,3 +25,47 @@ for (let anchor of anchors) {
   });
 }
 // -- Плавная прокрутка до элемента --
+
+// -- Обработка сабмита формы --
+function getInputValues(evt) {
+  const inputValues = {}
+  const form = evt.target.closest('.callback__form')
+  const inputList = form.querySelectorAll('.callback__form-field')
+
+  inputList.forEach((input) => {
+    inputValues[input.name] = input.value
+  })
+
+  return inputValues
+}
+
+function sendData(data) {
+  return fetch(`http://localhost:3000/api/send`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+}
+
+function submitHandle(evt) {
+  const data = getInputValues(evt)
+  console.log(data)
+
+  sendData(data)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+}
+
+const buttons = document.querySelectorAll('.callback__submit-btn')
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', evt => {
+    evt.preventDefault()
+    submitHandle(evt)
+  })
+})
+// - Обработка сабмита формы -
